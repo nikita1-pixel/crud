98f60d0fe3ab3f0f2d4b040534e8a179de12f9c8
Nikita Choudhary <84392685+nikita1-pixel@users.noreply.github.com>
2025-10-23 23:35:28 +0530

    connecting database

--- back/src/index.js
+++ back/src/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
+app.use(cors());//ENALBE CORS POLICY
+app.use(express.json());//WE'LL BE PASSING DATA IN JSON FORMAT
+//MIDLDLWARE TO PARSE JSON DATA
 
 //response to our get request
 app.get('/' , (req, res)=> {
